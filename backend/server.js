15906e656f920d6e948f8ea0e912a33bcef9a5e6
ASHUTOSH KUMAR SINGH <ashutoshs.ic18@nsut.ac.in>
2020-10-04 18:43:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const rateLimit = require("express-rate-limit");
 const app = express();
 
 app.use(morgan("common"));
-// app.use(helmet());
+app.use(helmet());
 app.use(cors());
 app.use(express.json())
 app.use(express.static(path.join(__dirname + "/../frontend/build"))); 
