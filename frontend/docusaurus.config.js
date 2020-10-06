d41b470b7fea13c82e821e3a70f451f2463c57c2
jadon <jadonwolffs@gmail.com>
2020-10-06 22:40:25 +0200

    added initial pwa support via docusaurus plugin

--- frontend/docusaurus.config.js
+++ frontend/docusaurus.config.js
@@ -1,4 +1,65 @@
 module.exports = {
+  plugins: [
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          "appInstalled",
+          "queryString",
+          "always",
+        ],
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/img/logo.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json", // your PWA manifest
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-capable",
+            content: "yes",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-status-bar-style",
+            content: "#000",
+          },
+          {
+            tagName: "link",
+            rel: "apple-touch-icon",
+            href: "/img/logo.png",
+          },
+          {
+            tagName: "link",
+            rel: "mask-icon",
+            href: "/img/logo.svg",
+            color: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileImage",
+            content: "/img/logo.png",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileColor",
+            content: "#000",
+          },
+        ],
+      },
+    ],
+  ],
   title: "Final Space API",
   tagline: `"That's Not A Cookie. And You Know That HUE, You Know That!"`,
   url: "https://finalspaceapi.com",
