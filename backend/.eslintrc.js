d357c7cb59d387b279df5b3cea3a74c1cbfa8839
Erick B. lEMOS <ericklemoscosta@gmail.com>
2020-10-08 16:38:49 -0300

    npm script and integration with prettier

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
         "es2021": true,
         "node": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "prettier"
+    ],
     "parserOptions": {
         "ecmaVersion": 12
     },
