a43dbda2f476e8c6ee9721ac179603bac3b53a97
PSmigielski <walek.smigielski@gmail.com>
2020-10-09 12:57:58 +0200

    add Vanilla js and axios static examples

--- frontend/sidebars.js
+++ frontend/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     "Getting Started": ["intro", "character", "episode"],
-    "Examples": ["react"],
+    "Examples": ["react", "javascript"],
     About: ["about"],
   },
 };
