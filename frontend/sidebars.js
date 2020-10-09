d9a38c1625cee9e2f401919679a9cc70353c4566
Tim Richter <tim@timrichter.dev>
2020-10-09 08:49:45 +0200

    Fixes #98: Add location endpoint docs

--- frontend/sidebars.js
+++ frontend/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    "Getting Started": ["intro", "character", "episode"],
+    "Getting Started": ["intro", "character", "episode", "location"],
     "Examples": ["react"],
     About: ["about"],
   },
