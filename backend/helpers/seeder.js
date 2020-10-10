55f0ae861b3ded3911e475252786ed139f69788a
Mohammed Alsubhi <m@mhh.sa>
2020-10-10 07:45:29 +0300

    Database seeder (#119)
    * add database seeder to backend (#64)
    
    * fix LGTM alerts (#64)
    
    * move seeder file to helpers folder
    
    * fix eslint errors

--- backend/helpers/seeder.js
+++ backend/helpers/seeder.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 require("dotenv").config({ path: __dirname + "/./../.env" })
 const mongoose = require("mongoose")
 const axios = require("axios")
@@ -5,14 +6,47 @@ const Character = require("../models/character.model")
 const Episode = require("../models/episode.model")
 const database_url = process.env.DATABASE_URL
 
-seed = async () => {
-  connectDb()
-  await seedCharacters()
-  await seedEpisodes()
-  closeDb()
+const seedCharacters = async () => {
+  const charactersEndpoint = "https://finalspaceapi.com/api/v0/character"
+  let characters = null
+  await axios
+    .get(charactersEndpoint)
+    .then((res) => (characters = res.data))
+    .then(() => console.log("Characters data Fetched"))
+    .catch((err) => console.log("Error: characters data didn't fetched " + err))
+
+  try {
+    for (var character of characters) {
+      const newCharacter = new Character(character)
+      await newCharacter.save()
+    }
+    console.log("Characters seeded to database successfully")
+  } catch (err) {
+    console.log("Error: characters didn't seeded to database " + err)
+  }
 }
 
-connectDb = async () => {
+const seedEpisodes = async () => {
+  const episodesEndpoint = "https://finalspaceapi.com/api/v0/episode"
+  let episodes = null
+  await axios
+    .get(episodesEndpoint)
+    .then((res) => (episodes = res.data))
+    .then(() => console.log("Episodes data Fetched"))
+    .catch((err) => console.log("Error: episodes data didn't fetched " + err))
+
+  try {
+    for (var episode of episodes) {
+      const newEpisode = new Episode(episode)
+      await newEpisode.save()
+    }
+    console.log("Episodes seeded to database successfully")
+  } catch (err) {
+    console.log("Error: episodes didn't seeded to database " + err)
+  }
+}
+
+const connectDb = async () => {
   mongoose
     .connect(database_url, {
       useNewUrlParser: true,
@@ -28,48 +62,15 @@ connectDb = async () => {
     })
 }
 
-closeDb = async () => {
+const closeDb = async () => {
   mongoose.connection.close()
 }
 
-seedCharacters = async () => {
-  let charactersEndpoint = "https://finalspaceapi.com/api/v0/character"
-  let characters
-  await axios
-    .get(charactersEndpoint)
-    .then((res) => (characters = res.data))
-    .then(() => console.log("Characters data Fetched"))
-    .catch((err) => console.log("Error: characters data didn't fetched " + err))
-
-  try {
-    for (var character of characters) {
-      let newCharacter = new Character(character)
-      await newCharacter.save()
-    }
-    console.log("Characters seeded to database successfully")
-  } catch (err) {
-    console.log("Error: characters didn't seeded to database " + err)
-  }
-}
-
-seedEpisodes = async () => {
-  let episodesEndpoint = "https://finalspaceapi.com/api/v0/episode"
-  let episodes
-  await axios
-    .get(episodesEndpoint)
-    .then((res) => (episodes = res.data))
-    .then(() => console.log("Episodes data Fetched"))
-    .catch((err) => console.log("Error: episodes data didn't fetched " + err))
-
-  try {
-    for (var episode of episodes) {
-      let newEpisode = new Episode(episode)
-      await newEpisode.save()
-    }
-    console.log("Episodes seeded to database successfully")
-  } catch (err) {
-    console.log("Error: episodes didn't seeded to database " + err)
-  }
+const seed = async () => {
+  connectDb()
+  await seedCharacters()
+  await seedEpisodes()
+  closeDb()
 }
 
 seed()
