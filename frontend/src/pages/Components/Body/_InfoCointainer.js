4e29981133819275967852491b356f1aea85ce06
ASHUTOSH KUMAR SINGH <ashutoshs.ic18@nsut.ac.in>
2020-10-13 15:41:36 +0530

    Update _InfoCointainer.js

--- frontend/src/pages/Components/Body/_InfoCointainer.js
+++ frontend/src/pages/Components/Body/_InfoCointainer.js
@@ -12,7 +12,7 @@ function InfoContainer(){
                 The series involves a prisoner named <b>Gary Goodspeed</b> and
                 his alien friend, <b>Mooncake</b>, and focuses on their
                 intergalactic adventures as they try to solve the mystery of the
-                titular "Final Space".<br />
+                titular &quot;Final Space&quot;.<br />
 
                 The data and images are used without claim of ownership and belong to their respective owners.
                 This API is open source and uses a <a href="https://github.com/lelouchB/final-space-api/blob/main/LICENSE" >BSD license</a>.
