c62bc78cdae2b796065db83379d6bfc50e5498fc
Rafael Guzmán Barranco <iam@guzbarraf.com>
2020-10-16 15:29:23 -0500

    Add status icon to Character endpoint #115 (works with 50x50px images, functionality with css is commented)

--- frontend/src/pages/Components/Body/_Card.js
+++ frontend/src/pages/Components/Body/_Card.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import styles from "../_styles.module.css";
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import '../../../css/_Card.css'
 
 function Card(props){
-    return(
+  const statusCharacter = (status) => {
+    switch(status) {
+      case 'Alive':
+        return 'green';
+      case 'Dead':
+      case 'Deceased':
+        return 'red';
+      case 'Operational':
+        return 'yellow';
+      default:
+        return 'black';
+    }
+  }
+
+  return(
         <div className={styles.card}>
             <div className="card shadow--tl">
             <div className="card__image">
@@ -16,6 +31,11 @@ function Card(props){
             <div className="card__body">
                 <h3>{props.name}</h3>
                 <small>{props.status}</small>
+
+               { /* <div className={`status-icon status-${statusCharacter(props.status)}`}></div> */ }
+                <div className="cont-img-status">
+                  <img src={`../../../img/icon-status-${statusCharacter(props.status)}.jpg`} alt={props.status}/>
+                </div>
             </div>
             </div>
         </div>
