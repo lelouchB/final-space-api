6a0dbb1b536a756dae5c76d4192de1683ee214f3
Rafael Guzmán Barranco <guzbarraf@hotmail.com>
2020-10-30 21:44:59 -0600

    Example in Angular and Axios (#138)
    * Example in Angular
    
    * Example in Angular - remove the tabs and add the code snippets
    
    * Example in Angular #134 - delete tabs and add the code snippets
    
    * Update angular.mdx
    
    * Update angular.mdx
    
    alt tag update
    
    * Add live example of the Final Space API using Angular
    
    * update angular.mdx
    
    Add live example of the Final Space API using Angular
    
    * update angular.mdx
    
    * Add of operation with Angular and Axios
    
    Co-authored-by: ASHUTOSH KUMAR SINGH <ashutoshs.ic18@nsut.ac.in>

--- frontend/sidebars.js
+++ frontend/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
-    "Getting Started": ["intro", "character", "episode", "location"],
-    Examples: ["react", "vuejs","javascript"],
+    "Getting Started": ["intro", "character", "episode"],
+    Examples: ["react", "angular"],
     About: ["about"],
   },
 }
