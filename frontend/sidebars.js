c9ff1ef801726c8faedf1d998d7cdbd34bc330e1
fullstackmafia <ugwuraphael@gmail.com>
2021-01-04 18:28:59 +0400

    new updates

--- frontend/sidebars.js
+++ frontend/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     "Getting Started": ["intro", "character", "episode", "location", "quote"],
-    Examples: ["react", "vuejs", "javascript", "angular", "slider"],
+    Examples: ["react", "vuejs", "javascript", "angular", "slider", "svelte"],
     About: ["about"],
   },
 }
